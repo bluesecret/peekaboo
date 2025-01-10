e3323e94db327873550f6215f6b79edca26ada11
wangk <clearstyle@163.com>
2025-01-10 11:38:04 +0800

    [Feature][Server] Add main

---
+++ peekaboo-server/peekaboo-app/src/main/java/io/wangk/peekaboo/server/app/PeekabooApplication.java
@@ -0,0 +1,11 @@
+package io.wangk.peekaboo.server.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeekabooApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PeekabooApplication.class, args);
+    }
+}
